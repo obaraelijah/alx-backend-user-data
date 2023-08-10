d7c9eae36f02e3895d0f53bc77de80ca0c8fc1bb
obaraelijah <elijahobara357@gmail.com>
2023-08-10 10:33:19 +0300

    unauthorized 401

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,9 +16,15 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """Not found handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
