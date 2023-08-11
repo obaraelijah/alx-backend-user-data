90584126c2e8fb28998a52d34e11966d4e5b0478
obaraelijah <elijahobara357@gmail.com>
2023-08-11 10:24:11 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,9 +9,17 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+from api.v1.auth.auth import Auth
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+
 
 
 @app.errorhandler(404)
@@ -37,3 +45,22 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+    
+@app.before_request
+def authenticate_user():
+    """Authenticates a user before processing a given request.
+    """ 
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',  '/api/v1/forbidden/',]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)    
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)  
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,6 +28,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Gets the authorization header field from the request.
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
