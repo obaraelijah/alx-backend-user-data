d2715e5dfcefa9563fe4895c5bfd1585f14ec76a
obaraelijah <elijahobara357@gmail.com>
2023-08-14 09:49:56 +0300

    session auth

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -15,6 +15,27 @@ def view_all_users() -> str:
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def view_one_user(user_id: str = None) -> str:
+    """GET /api/v1/users/:id
+    Path parameter:
+      - User ID.
+    Return:
+      - User object JSON represented.
+      - 404 if the User ID doesn't exist.
+    """
+    if user_id is None:
+        abort(404)
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        else:
+            return jsonify(request.current_user.to_json())
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
