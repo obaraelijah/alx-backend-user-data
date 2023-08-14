90bda384caa0b246a107d5f6f42c0dc8183a706e
obaraelijah <elijahobara357@gmail.com>
2023-08-14 10:27:02 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
-
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
-
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+if auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
+if auth_type == 'session_db_auth':
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -33,18 +37,19 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
-def Unauthorized(error) -> str:
+def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """forbidden handler.
+    """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 
 @app.before_request
@@ -53,20 +58,22 @@ def authenticate_user():
     """
     if auth:
         excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
-                abort(403) 
-            request.current_user = user  
+                abort(403)
+            request.current_user = user
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)  
+    app.run(host=host, port=port)
--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""Module of Index views.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,23 +7,24 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
-      - the status of the API
+      - the status of the API.
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each objects.
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
@@ -32,7 +33,8 @@ def unauthorized() -> None:
       - Unauthorized error.
     """
     abort(401)
-    
+
+
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
     """GET /api/v1/forbidden
@@ -40,5 +42,3 @@ def forbidden() -> None:
       - Forbidden error.
     """
     abort(403)
-    
-    
