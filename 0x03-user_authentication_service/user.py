321a59bfd3c3fead1511c382638c867354d9111a
obaraelijah <elijahobara357@gmail.com>
2023-08-15 11:54:04 +0300

    User model fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,9 +3,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
-
 
 class User(Base):
     """Represents a record from the `user` table."""
