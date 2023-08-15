8825ec0726c0443c7470698c6ef0404bebde0a52
obaraelijah <elijahobara357@gmail.com>
2023-08-15 13:57:34 +0300

    Log in sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,6 +35,21 @@ def users() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
     
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    Return:
+        - The account login payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+    
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
