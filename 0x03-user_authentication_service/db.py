5f23071305b589abf0f0cebdb5bc0f6d28f1aff1
obaraelijah <elijahobara357@gmail.com>
2023-08-15 12:09:30 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,3 +48,14 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id."""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
+        return None
