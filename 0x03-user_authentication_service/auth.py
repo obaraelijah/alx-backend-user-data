78bf9afadc616e58fde64fde90f368cace3e929f
obaraelijah <elijahobara357@gmail.com>
2023-08-15 13:17:43 +0300

    Register user

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A module for authentication-related routines."""
+import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password."""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generates a UUID."""
+    return str(uuid4())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initializes a new Auth instance."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Adds a new user to the database."""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's login details are valid."""
+        user = None
